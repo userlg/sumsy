e8b9d58f4826a800f8538526fee090b9647a26d6
userlg <guipeluis90@gmail.com>
2025-08-24 20:45:45 -0400

    New changes

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,39 +1,35 @@
 /// <reference types="vitest" />
 
-import { defineConfig } from 'vitest/config'
-import vue from '@vitejs/plugin-vue'
-import path from 'path'
-
+import { defineConfig } from 'vitest/config';
+import vue from '@vitejs/plugin-vue';
+import path from 'path';
 
 export default defineConfig({
-    plugins: [
-        vue(),
-    ],
-    resolve: {
-        alias: {
-            '@': path.resolve(__dirname, './src'),
-            '@test': path.resolve(__dirname, './tests'),
-        },
+  plugins: [vue()],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+      '@test': path.resolve(__dirname, './tests'),
     },
-    test: {
-        globals: true,
-        watch: false,
-        root: './',
-        environment: 'happy-dom',
-        setupFiles: './vitest.setup.ts',
-        include: ['tests/**/*.test.ts', 'tests/**/*.test.tsx', 'tests/**/*.test.vue'],
-        coverage: {
-            provider: 'v8',
-            include: ['src/**/*.{ts,tsx,js,jsx,vue}'],
-            exclude: [
-                'tests/',
-                '**/*.d.ts',
-                '**/vitest.setup.ts',
-                '**/*.test.*',
-                'node_modules/',
-                'src/main.ts',
-                'src/scripts/run-reset-app.ts',
-            ],
-        }
+  },
+  test: {
+    globals: true,
+    watch: false,
+    root: './',
+    environment: 'happy-dom',
+    include: ['tests/**/*.test.ts', 'tests/**/*.test.tsx', 'tests/**/*.test.vue'],
+    coverage: {
+      provider: 'v8',
+      include: ['src/**/*.{ts,tsx,js,jsx,vue}'],
+      exclude: [
+        'tests/',
+        '**/*.d.ts',
+        '**/vitest.setup.ts',
+        '**/*.test.*',
+        'node_modules/',
+        'src/main.ts',
+        'src/scripts/run-reset-app.ts',
+      ],
     },
-})
+  },
+});
